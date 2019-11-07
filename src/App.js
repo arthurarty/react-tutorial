51e454921c44dfd0d4201ecc51d4d8cf621fbd85
arthurarty <arthur.nangai@gmail.com>
2019-11-07 11:18:43 +0200

    ft(state) - introduce use of state.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charles',
         job: 'Janitor'
@@ -24,10 +24,25 @@ class App extends Component {
         name: 'Denis',
         job: 'Bartender'
       },
-    ]
+    ],
+  };
+
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    })
+  }
+
+  render() {
+    const { characters } = this.state
+
     return (
       <div className="Container">
-          <Table characterData={characters} />
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
